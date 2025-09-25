703b9289163419f43d34418c88462b5821d6bbbd
Lucas <lucas25070111@aluno.cesupa.br>
2025-09-25 17:00:31 -0300

    melhorando o menu principal e adicionando comentários e a área "acadêmica"

--- Trabalho DP/main.c
+++ Trabalho DP/main.c
@@ -4,15 +4,20 @@
 
 int main(int argc, char const *argv[])
 {
+    //variaveis
+    int nota1, nota2, nota3, nota4;
+    int soma, media, dobro;
     int escolha, idade;
     float peso, altura, imc;
 
+    //posição do goto PRONTO
     inicio:
-
-    printf("MENU principal\n1-pessoal\n2-financeiro\n3-academico\n4-utilidades\n0-sair\n");
+    //menu principal PRONTO
+    printf("\n\t\t\t---MENU PRINCIPAL---\n\t\t\t     1-pessoal\n\t\t\t     2-financeiro\n\t\t\t     3-academico\n\t\t\t     4-utilidades\n\t\t\t     0-sair\n");
         scanf("%i", &escolha);
 
         switch (escolha){
+            //area pessoal PRONTA
         case 1:
             printf("bem vindo a area pessoal\ndigite sua idade\n");
                 scanf("%i", &idade);
@@ -32,25 +37,59 @@ int main(int argc, char const *argv[])
                     else if (idade == 16||17 || idade > 70){
                         printf("\nseu voto opcional");
                     }
+                    goto inicio;
                         break;  
+        //area financeira
         case 2:
-            printf("bem vindo a area financeira");
+            printf("bem vindo a area financeira\n");
+
+            goto inicio;
                 break;
+        //area academica PRONTA
         case 3:
-            printf("bem vindo a area academica");
-                break;
+            printf("bem vindo a area academica\n");
+                printf("digite sua primeira nota\n");
+                    scanf("%i", &nota1);
+                        printf("\ndigite sua segunda nota\n");
+                            scanf("%i", &nota2);
+                                printf("\ndigite sua terceira nota\n");
+                                    scanf("%i", &nota3);
+                                        printf("\ndigite sua quarta nota\n");
+                                            scanf("%i", &nota4);
+                                            
+            soma = nota1+nota2+nota3+nota4;
+            media = soma/4;
+            dobro = media*2;
+
+        printf("\nsua soma das notas e igual a: %i, a media e igual a %i e o dobro da media e igual a: %i", soma, media, dobro);
+
+        if (media >= 7){
+            printf("\n sua media foi %i voce esta aprovado", media);
+        }
+        else if (media >= 4 && media <= 7){
+            printf("\nsua media foi %i voce esta de recuperacao", media);
+        }
+        else{
+            printf("\n sua media foi %i voce esta reprovado", media);
+        }
+        goto inicio;
+            break;
+            //area de utilidades
         case 4:
-            printf("bem vindo a area utilidades");
+            printf("bem vindo a area utilidades\n");
+            goto inicio;
                 break;
+                //sair do sistema PRONTA
         case 0:
-            printf("obrigado pelo acesso");
+            printf("obrigado pelo acesso\n");
                 goto fim;
+                //alerta e retorno para o menu principal PRONTA
         default:
         printf("escolha invalida, por favor selecione um dos numeros\n");
             goto inicio;
         }
         
-    
+    //saida do codigo
         fim:
         
     return 0;
